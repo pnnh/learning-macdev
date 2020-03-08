58bccab39c5233ef366fe51c4940196346b59d78
larry <larry@yalla.live>
2020-03-08 21:24:00 +0800

    Initial Commit

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Larry on 2020/3/8.
+//  Copyright © 2020 larry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
